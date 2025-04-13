43e4e334cc37d6f48e7c68b61f407af44d7f8ec6
Andre Ratzenberger <andre.ratzenberger@whiteduck.de>
2025-04-13 14:46:34 +0200

    init

--- examples/playground/02_cook_book/save_and_load/load_04.py
+++
@@ -1,9 +0,0 @@
-
-
-from flock.core.flock import Flock
-
-
-flock = Flock.load_from_file("flock.json")
-agent = flock.agents
-
-flock.run(start_agent=agent["bloggy"], input={"blog_idea": "A blog about cats"})
