4366f6e6335cb88f03e2ece288afcd5eb45db2b4
Andre Ratzenberger <andre.ratzenberger@whiteduck.de>
2025-04-14 00:19:48 +0200

    fix show_banner

--- src/flock/core/flock.py
+++ src/flock/core/flock.py
@@ -150,8 +150,7 @@ class Flock(BaseModel, Serializable):
                     )
 
         # Initialize console if needed for banner
-        if self.show_flock_banner:  # Use instance attribute
-            init_console()
+        init_console(clear_screen=True, show_banner=self.show_flock_banner)
 
         # Set Temporal debug environment variable
         self._set_temporal_debug_flag()
